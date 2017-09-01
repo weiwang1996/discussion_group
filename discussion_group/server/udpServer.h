a65f40f7448703d12dd30abeabe32d1ec683843b
weiwang <1395251934@qq.com>
2017-09-01 11:12:08 +0800

    Update udpServer.h

--- discussion_group/server/udpServer.h
+++ discussion_group/server/udpServer.h
@@ -15,6 +15,7 @@
 #include <pthread.h>
 #include "dataType.h"
 
+//定义udp服务器类
 class udpServer
 {
 public:
@@ -26,15 +27,17 @@ public:
     int brocast();
     ~udpServer();
 private:
+    //拒绝拷贝构造和赋值
     udpServer(const udpServer&);
-    void addUser(const struct sockaddr_in &remote);
-    void delUser(const struct sockaddr_in &remote);
+    udpServer& operator=(const udpServer&);
+    void addUser(const struct sockaddr_in &remote);//添加用户
+    void delUser(const struct sockaddr_in &remote);//删除用户
 private:
-    int sock;
-    std::string ip;
-    int port;
-    std::map<in_addr_t,struct sockaddr_in>  user_list;
-    data_pool  pool;
+    int sock;//socket
+    std::string ip;//IP地址 （字符串类型）
+    int port;//端口号
+    std::map<in_addr_t,struct sockaddr_in>  user_list;//存储用户的map
+    data_pool  pool;//数据池
 };
 
 #endif
