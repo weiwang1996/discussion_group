569c640253b5a7d77adced0b4c17339f1b645d43
weiwang <1395251934@qq.com>
2017-09-01 15:17:42 +0800

    Update window.h

--- discussion_group/window/window.h
+++ discussion_group/window/window.h
@@ -6,18 +6,20 @@
 
 class window{
 public:
+    //创建window 句柄
     window();
-    void wrefreshMyWindow(WINDOW* win);
-    void putStringToWin(WINDOW* win);
-    void putStringToWin(WINDOW* win,int start_y,int start_x,std::string &msg);
-    void getStringFromWin(WINDOW* win,std::string &outString);
-    void clrNumsLines(WINDOW* win,int begin,int nums);
+    void wrefreshMyWindow(WINDOW* win);//清屏
+    void putStringToWin(WINDOW* win);//给window上输出字符串
+    void putStringToWin(WINDOW* win,int start_y,int start_x,std::string &msg);//
+    void getStringFromWin(WINDOW* win,std::string &outString);//从window上拿到消息下来
+    void clrNumsLines(WINDOW* win,int begin,int nums);//清理一行内容
     void createHeader();
     void createoutput();
     void createfriendList();
     void createinput();
     ~window();
 public:
+    //管理四个窗口
     WINDOW* header;
     WINDOW* output;
     WINDOW* friend_list;
