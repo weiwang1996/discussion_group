86c573409414d8355a93ca92659a0687efb1eb4c
weiwang <1395251934@qq.com>
2017-09-01 12:13:43 +0800

    Update jsonApi.cpp

--- discussion_group/comm/jsonApi.cpp
+++ discussion_group/comm/jsonApi.cpp
@@ -10,9 +10,9 @@ int jsonApi::serialize(const Json::Value &val,std::string &outString)
 {
 #ifdef _FAST_
     Json::FastWriter w;
-    outString = w.write(val);
+    outString = w.write(val);//Fast模型序列化 把很多字符串转化为{"name":"乔布斯","campany":"Apple","age":"12"}
 #else
-    Json::StyledWriter w;
+    Json::StyledWriter w;//Style模型序列化
     outString = w.write(val);
 #endif
     return 0;
@@ -20,7 +20,7 @@ int jsonApi::serialize(const Json::Value &val,std::string &outString)
 int jsonApi::deSerialize(std::string &inString,Json::Value &val)
 {
     Json::Reader r;
-    if(r.parse(inString,val,false)<0)
+    if(r.parse(inString,val,false)<0)//反序列化
     {
     return 0;
     }
